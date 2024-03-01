6cc78805361263bc046ca402b6a9bbad09269feb
yi <yi.dai@eeoa.com>
2024-03-01 15:29:24 +0800

    update test_dict

--- egs/attention_aug/dict/test_dict.py
+++ egs/attention_aug/dict/test_dict.py
@@ -1,6 +1,7 @@
 from g2p_en import G2p
 import string
 from phonemizer import phonemize
+from phonemizer.backend import EspeakBackend
 # from g2p import make_g2p
 
 # Use a pipeline as a high-level helper
@@ -91,7 +92,8 @@ for phone in cmu_phones:
         print(phone, p1, p2)
 
 g2p = G2p()
-transducer = make_g2p('dan', 'eng-arpabet')
+backend = EspeakBackend('en-us')
+# transducer = make_g2p('dan', 'eng-arpabet')
 
 def look_up_ipa_dict(word):
     l = cmudict_ipa.get(word, None)
@@ -108,7 +110,7 @@ def use_g2p_en(word):
     return "".join(phones)
 
 def use_phonemizer(word):
-    return word
+    return backend.phonemize([word])[0]
 
 def look_up_cmu_dict(word):
     return word
@@ -117,25 +119,18 @@ def look_up_cmu_dict(word):
 words = ["about", "through", "rough", "cough", "ought", "magazine", "hurt", "but", "accept", "2", "talked", "bananas", "wishes", "OPPO"]
 
 for word in words:
-    print(word, look_up_ipa_dict(word), use_g2p_en(word))
+    print(word, look_up_ipa_dict(word), use_g2p_en(word), use_phonemizer(word))
 
-exit()
+# exit()
 
 # sentence
 texts = ["I have $250 in my pocket.", # number -> spell-out
          "popular pets, e.g. cats and dogs", # e.g. -> for example
          "I refuse to collect the refuse around here.", # homograph
-         "I'm an activationist."] # newly coined word
-words = ["hurt", 'bananas', 'unquote', "the apple of my eye at 7 am"]
-for word in words:
-    print(g2p(word))
-    # print(pipe(word))
-
-    # print(transducer(word).output_string)
-
-    # out = phonemize(word, backend='espeak', language= 'en-us')
-    # print(out)
-# >>> ['AY1', ' ', 'HH', 'AE1', 'V', ' ', 'T', 'UW1', ' ', 'HH', 'AH1', 'N', 'D', 'R', 'AH0', 'D', ' ', 'F', 'IH1', 'F', 'T', 'IY0', ' ', 'D', 'AA1', 'L', 'ER0', 'Z', ' ', 'IH0', 'N', ' ', 'M', 'AY1', ' ', 'P', 'AA1', 'K', 'AH0', 'T', ' ', '.']
-# >>> ['P', 'AA1', 'P', 'Y', 'AH0', 'L', 'ER0', ' ', 'P', 'EH1', 'T', 'S', ' ', ',', ' ', 'F', 'AO1', 'R', ' ', 'IH0', 'G', 'Z', 'AE1', 'M', 'P', 'AH0', 'L', ' ', 'K', 'AE1', 'T', 'S', ' ', 'AH0', 'N', 'D', ' ', 'D', 'AA1', 'G', 'Z']
-# >>> ['AY1', ' ', 'R', 'IH0', 'F', 'Y', 'UW1', 'Z', ' ', 'T', 'UW1', ' ', 'K', 'AH0', 'L', 'EH1', 'K', 'T', ' ', 'DH', 'AH0', ' ', 'R', 'EH1', 'F', 'Y', 'UW2', 'Z', ' ', 'ER0', 'AW1', 'N', 'D', ' ', 'HH', 'IY1', 'R', ' ', '.']
-# >>> ['AY1', ' ', 'AH0', 'M', ' ', 'AE1', 'N', ' ', 'AE2', 'K', 'T', 'IH0', 'V', 'EY1', 'SH', 'AH0', 'N', 'IH0', 'S', 'T', ' ', '.']
+         "I'm an activationist.",
+         "The apple of my eye at 7 am run off."
+         ] # newly coined word
+for sentence in texts:
+    print(sentence)
+    print(use_g2p_en(sentence))
+    print(use_phonemizer(sentence))
