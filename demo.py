6c976b202458062b16736e5ba0c9f6c63a97be91
daiyi <daiyi@eeoa.com>
2024-05-09 17:21:49 +0800

    update demo

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ from scoring.pronunciation_assessment import PronunciationAssessment
 from dict.phonetic_dict import Phonetic
 
 def main():
-    t0 = time.time()
+    start = time.time()
     system = platform.system()
     if system == 'Darwin':
         os.environ['PHONEMIZER_ESPEAK_LIBRARY'] = '/opt/homebrew/Cellar/espeak/1.48.04_1/lib/libespeak.dylib'
@@ -35,20 +35,29 @@ def main():
     
     word_cnt = 0
     character_cnt = 0
-    start = time.time()
-    print(start - t0)
+    for wav, txt in mixed.items():
+        word_cnt += len(txt.split(' '))
+        character_cnt += len(txt)
+
+    t0 = time.time()
     for wav, txt in mixed.items():
         json_file = wav.replace('.wav', '.json')
         if os.path.exists(json_file):
             os.remove(json_file)
         print(phonetic.api_all_in_one(txt, to_json=True, json_file=json_file))
+    
+    t1 = time.time()
+
+    for wav, txt in mixed.items():
+        json_file = wav.replace('.wav', '.json')
         print(assessment.assess_text(txt, wav, to_json=True, json_file=json_file))
 
-        word_cnt += len(txt.split(' '))
-        character_cnt += len(txt)
-
-    print(word_cnt, character_cnt)
-    print(time.time() - start)
+    t2 = time.time()
+    print('word number: ',  word_cnt)
+    print('character number: ', character_cnt)
+    print('init time:', t0 - start)
+    print('dict time:', t1 - t0)
+    print('assessment time:', t2 - t1)
 
     return 0
 
